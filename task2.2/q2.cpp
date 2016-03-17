4014a8384a17f8e62475c8e59f338e74e1fd27f4
andreistock <andreihirjanu@gmail.com>
2016-03-17 00:11:16 +0000

    task2 with new version of q1

---
+++ task2.2/q2.cpp
@@ -0,0 +1,106 @@
+#include "trimatrix.hpp"
+#include <vector>
+#include <algorithm>
+#include <math.h>
+void print(double k)
+{
+	std::cout<<k<<" ";
+}
+void set_boundary_conditions(std::vector<double> &u0,double &L,int &Nx)
+{
+	double k;
+	for (double i=0;i<=Nx;i++)
+	{
+		k=i*L/(double)Nx;
+//		std::cout<<k<<"\n";
+		u0[i]=k*(1-k);
+	}
+}
+void display_vector(std::vector<double> &uk)
+{
+	for_each(uk.begin(),uk.end(),print);
+	std::cout<<"\n";
+}
+void calculate(std::vector<double> &u0,std::vector<double> &u,int &Nx,double &gamma1,double &gamma2,trimatrix &bar)
+{
+	u[0]=gamma1;
+	u[Nx]=gamma2;
+	double sum=0;
+	for (int i=1;i<Nx;i++){
+		sum=0;
+		for(int j=0;j<Nx;j++){
+			if(i==j)
+			{
+				sum=sum+bar.mDiag[i]*u0[i];
+			}
+			else if(j==i-1)
+			{
+				sum=sum+bar.mLower[j]*u0[i-1];
+			}
+			else if(j==i+1)
+			{
+				sum=sum+bar.mUpper[j]*u0[i+1];
+			}
+		}
+		u[i]=sum;
+	}
+	u0=u;
+}
+void full_calculation(double &T,
+					  double &delta_t,std::vector<double> &u0,
+					  std::vector<double> &u,
+					  int &Nx,double &gamma1,double &gamma2,trimatrix &bar)
+{
+
+	for(double i=0;i<T;i+=delta_t)
+	{
+		calculate(u0,u,Nx,gamma1,gamma2,bar);
+		std::cout<<u0[Nx/2]<<" ";
+	}
+}
+int numbers()
+{
+	return 1;
+}
+int main(int argc, char* argv[])
+{
+  	double L=atof(argv[1]);
+	int Nx=atoi(argv[2]);
+	double T=atof(argv[3]);
+	int Nt=atoi(argv[4]);
+	double alpha=atof(argv[5]);
+	double gamma1=atof(argv[6]);
+	double gamma2=atof(argv[7]);
+	double delta_t,delta_x,niu;
+	std::vector<double> u,u0;
+	u0.resize(Nx+1);
+	u.resize(Nx+1);
+	delta_t=T/(double)Nt;
+	delta_x=L/(double)Nx;
+	niu=alpha*delta_t/(pow(L/delta_x,2));
+
+	// std::cout<<"Please input length of bar: ";
+	// std::cin>>L;
+	// std::cout<<"Please input number Nx: ";
+	// std::cin>>Nx;
+	// std::cout<<"Please input evaluated time T: ";
+	// std::cin>>T;
+	// std::cout<<"Please input number Nt: ";
+	// std::cin>>Nt;
+	// std::cout<<"please input heat coefficient alpha: ";
+	// std::cin>>alpha;
+
+	trimatrix *bar = new trimatrix(Nx,niu,niu,1-2*niu,gamma1,gamma2);
+//	bar->display_matrix();
+	set_boundary_conditions(u0,L,Nx);
+//	display_vector(u0);
+//	calculate(u0,u,Nx,gamma1,gamma2,bar);
+//	display_vector(u0);
+//	calculate(u0,u,Nx,gamma1,gamma2,bar);
+//	display_vector(u0);
+	full_calculation(T,delta_t,u0,u,Nx,gamma1,gamma2,*bar);
+//	display_vector(u0);
+	delete bar;
+//	bar.display_vector(bar.u);
+//	bar.display_vector(bar.u0);
+}
---
+++ task2.2/q2.py
@@ -0,0 +1,37 @@
+import subprocess,sys
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pylab
+
+def run_command(command):
+    return subprocess.Popen(command,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT).communicate()
+
+
+subprocess.call(["ls", "-l"])
+Nx=20
+T=100000
+Nt=500
+T1=100000
+Nt1=5000
+
+x = np.linspace(0, T, Nt, endpoint=True)
+x1 = np.linspace(0,T1,Nt1,endpoint=True)
+
+command = "./q2 1 {} {} {} 1 0 0".format(Nx,T,Nt).split()
+command1 = "./q2 1 {} {} {} 1 0 0".format(Nx,T1,Nt1).split()
+abc = run_command(command)[0].split()
+abc1 = run_command (command1)[0].split()
+# for symbol in abc:
+# 	print symbol
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
+axes[0].plot(x,abc)
+axes[1].plot(x1,abc1)
+fig.tight_layout() 
+pylab.show()
+
+
+
+#pipe = Popen("./q2 1 10 10000 1000 1 0 0", shell=True, bufsize=bufsize, stdout=PIPE).stdout
